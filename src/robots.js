88c940f0f13eeab598b782b7a56f26df4e78595a
Qba <qba.skupinski@gmail.com>
2018-01-19 19:36:37 +0100

    'Robofriends'

---
+++ src/robots.js
@@ -0,0 +1,73 @@
+export const robots = [
+
+{
+    id: 1,
+    name: 'Michal Garbarczyk',
+    username: 'Garbi',
+    email: 'garbi_m@gmail.com'
+},
+
+{
+    id: 2,
+    name: 'Mateusz Grzegorowski',
+    username: 'Matysek',
+    email: 'matysek_m@gmail.com'
+},
+
+{
+    id: 3,
+    name: 'Artur Teodorczyk',
+    username: 'Arczi',
+    email: 'teo_a@gmail.com'
+},
+
+{
+    id: 4,
+    name: 'Michal Gielniak',
+    username: 'Magiel',
+    email: 'magiel_g@gmail.com'
+},
+
+{
+    id: 5,
+    name: 'Bartosz Plejsner',
+    username: 'Pleja',
+    email: 'pleja_b@gmail.com'
+},
+
+{
+    id: 6,
+    name: 'Jakub Nowicki',
+    username: 'Snajper',
+    email: 'snajper_q@gmail.com'
+},
+
+{
+    id: 7,
+    name: 'Mateusz Orzelek',
+    username: 'Orzel',
+    email: 'Orzel_m@gmail.com'
+},
+
+{
+    id: 8,
+    name: 'Oskar Kampioni',
+    username: 'Kampek',
+    email: 'kampek_o@gmail.com'
+},
+
+{
+    id: 9,
+    name: 'Wojciech Staniszewski',
+    username: 'Wojtas',
+    email: 'wojtek_g@gmail.com'
+},
+
+{
+    id: 10,
+    name: 'Kasia Kowalska',
+    username: 'Kaska',
+    email: 'kasia_k@gmail.com'
+},
+
+]
